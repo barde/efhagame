585fa5a06c893a29ef6ccf59043b8ae68b53259e
Bartholomäus Dedersen <git@selbstapotheose.de>
2012-06-05 13:18:45 +0200

    reading csv data in ai

--- ai.py
+++ ai.py
@@ -6,7 +6,12 @@
 #training from expected outputs
 
 from pybrain import *
+import os.path
 import optparse
+import csv
+import numpy
+import pprint
+from decimal import *
 
 def DEBUG(msg):
     if(options.debugMode):
@@ -14,39 +19,34 @@ def DEBUG(msg):
 
 class AI():
     def __init__(self,inputFile=False):
-#Object variables for initialisation
-        self.verbose = verbose
+    #Object variables for initialisation
+        self.inputFile = inputFile
 
         if self.inputFile:
             if not os.path.exists(inputFile):
                 print "File for CSV parsing does not exist!"
                 return
-            savedData = readCSVData(self.inputFile)
+            savedData = self.readCSVData(self.inputFile)
             
 
 
-    def readCSVData(inputFile):
+    def readCSVData(self,inputFile):
             lines = sum(1 for line in open(inputFile))
             with open(inputFile, 'rb') as f:
                 reader = csv.reader(f)
                 valuesRead = 0
-                values = zeros(lines,7)
+#init the multidimensional array
+                values = numpy.zeros((lines,7))
                 j = 0
                 for row in reader:
                     i = 0
                     for cell in row:
                         if i == 6:
-                            e6Value = Decimal(cell) * 100
-                            values[j][i] = long(e6Value)
-                            DEBUG("new line")
-                            DEBUG("values read: ")
-                            DEBUG(valuesRead)
+                            values[j][i] = Decimal(cell)
                             valuesRead += 1
                             i = 0
                         else:
-                            DEBUG("reading")
-                            e6Value = Decimal(cell) * 100
-                            values[j][i] = long(e6Value)
+                            values[j][i] = Decimal(cell)
                             i += 1
                     j += 1
 
@@ -64,10 +64,10 @@ if __name__ == '__main__':
      description = "Neural Network for generating randomness depending on user's brain activity",
      epilog = """ Best used in combination with the default loadout of libraries in the original project
             """)
-    parser.add_option("-v", "--verbose",
-        dest = "verbose",
+    parser.add_option("-d", "--debug",
+        dest = "debugMode",
         action = "store_true",
-        help = "be more verbose",
+        help = "show debug messages",
         default = False)
 
     parser.add_option("-i", "--input",
@@ -78,4 +78,4 @@ if __name__ == '__main__':
     dest="inputFile")
 
     (options, args) = parser.parse_args()
-    ai = AI(options.verbose,options.inputFile)
+    ai = AI(options.inputFile)
